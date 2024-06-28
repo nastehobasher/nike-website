232a08ff6cb31c947139406d4e9a177963a81c00
nasteho <nasteho764@gmail.com>
2024-06-28 22:26:21 +0300

    complete hero page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import Button from '../Components/Button'
 import {arrowRight} from '../assets/icons'
+import {bigShoe1} from '../assets/images'
 import { statistics } from '../constant'
 const Hero = () => {
   return (
@@ -11,18 +12,29 @@ const Hero = () => {
       <p className='font-montserrat text-xl text-coral-red'> Our Summer collections </p>
       <h1 className='mt-10 text-8xl font-palanquin max-sm:text-[72px] max-sm:leading-[82] font-bold'>
         <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The new arival</span><br />
-        <span className='text-coral-red mt-3'>Nike</span> shoes
+        <span className=' text-coral-red mt-3'>Nike</span> shoes
       </h1>
-      <p> Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+      <p className='font-montserrat text-slate-gray mt-6 mb-14 leading-8 sm:max-w-sm'> Discover stylish Nike arrivals, quality 
+        comfort, and innovation for your active life.</p>
       <Button label='Show Now' iconURl={arrowRight}/>
       <div className='flex justify-starts items-center flex-wrap mt-20 w-full  gap-16'>
         {statistics.map((stat)=>(
           <div key={stat.label}>
-            <p>{stat.value}</p>
-            <p>{stat.label}</p>
+            <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+            <p className='text-slate-gray font-montserrat leading-7'>{stat.label}</p>
           </div>
         ))}
       </div>
+    </div>
+
+    <div className='relative flex-1 flex justify-center items-center  xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+      <img 
+      src={bigShoe1}
+      alt="shoe collection"
+      className='object-contain relative z-10'
+      width={610}
+      height={500}
+       />
     </div>
    </section>
   )
